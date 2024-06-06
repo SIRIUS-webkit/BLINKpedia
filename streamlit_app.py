55b2abdab8d7a549a9903c2812972f882d5fcc44
La Min Ko Ko <lamin.koko@techcombine.co>
2024-06-07 00:33:56 +0700

    update temperature value

--- streamlit_app.py
+++ streamlit_app.py
@@ -96,7 +96,7 @@ def generate_format_prompt(input):
 
 def generate_llama2_response(prompt_input):
     format_prompt = generate_format_prompt(prompt_input)
-    llm =  HuggingFaceHub(repo_id="unsloth/tinyllama-chat", model_kwargs={"temperature":0.2,})
+    llm =  HuggingFaceHub(repo_id="unsloth/tinyllama-chat", model_kwargs={"temperature":0.1,})
     output = llm.invoke(format_prompt)
 
     return output
